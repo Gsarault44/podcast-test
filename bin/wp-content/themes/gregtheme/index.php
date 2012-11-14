a5afb47e52bcfe2d4b47ae88b4988585b0adb0fb
DANIEL MALL <dan@dantop.home>
2012-11-14 17:03:48 -0500

    changes made nov 14

--- bin/wp-content/themes/gregtheme/index.php
+++ bin/wp-content/themes/gregtheme/index.php
@@ -14,11 +14,7 @@
  */
 
 get_header(); ?>
-<?php
-$page_id = 2; // substitute page_id of page you want content included from for "2"
-$page = get_post($page_id);
-echo "<p>$page->post_content</p>";
-?>
+
 		<div id="container" >
 			<div id="content" role="main">
 				
